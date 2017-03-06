5621b344214a2f47adc578eee2d3e10240bdfcb9
KiYugadgeter <zyouhousikaku@gmail.com>
2017-03-06 13:47:21 +0900

    chapter 6 を更新

--- sample_codes/chapter06/cgi-bin/httphandler.py
+++ sample_codes/chapter06/cgi-bin/httphandler.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # 標準モジュールをimportする
 import cgi
 import os
+
 
 class Request(object):
     """
@@ -18,8 +19,9 @@ class Request(object):
         インスタンスの初期化メソッド
         クエリ，環境変数をアトリビュートとして保持する
         """
-        self.form=cgi.FieldStorage()
-        self.environ=environ
+        self.form = cgi.FieldStorage()
+        self.environ = environ
+
 
 import time
 
@@ -27,6 +29,7 @@ _weekdayname = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 _monthname = [None,
               "Jan", "Feb", "Mar", "Apr", "May", "Jun",
               "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 
 class Response(object):
     """
@@ -40,16 +43,17 @@ class Response(object):
         インスタンスの初期化メソッド
         ヘッダ用の辞書，本文用の文字列などを初期化する
         """
-        self.headers={'Content-type':'text/html;charset=%s' % charset}
-        self.body=""
-        self.status=200
-        self.status_message=''
+        self.headers = {
+            'Content-type': 'text/html;charset={:s}'.format(charset)}
+        self.body = ""
+        self.status = 200
+        self.status_message = ''
 
     def set_header(self, name, value):
         """
         レスポンスのヘッダを設定する
         """
-        self.headers[name]=value
+        self.headers[name] = value
 
     def get_header(self, name):
         """
@@ -61,7 +65,7 @@ class Response(object):
         """
         レスポンスとして出力する本文の文字列を返す
         """
-        self.body=bodystr
+        self.body = bodystr
 
     def make_output(self, timestamp=None):
         """
@@ -70,12 +74,12 @@ class Response(object):
         if timestamp is None:
             timestamp = time.time()
         year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
-        dtstr="%s, %02d %3s %4d %02d:%02d:%02d GMT" % (
+        dtstr = "{:s}, {:02d} {:3s} {:4d} {:02d}:{:02d}:{:02d} GMT".format(
             _weekdayname[wd], day, _monthname[month], year, hh, mm, ss)
         self.set_header("Last-Modified", dtstr)
-        headers='\n'.join(["%s: %s" % (k, v)
-                        for k,v in self.headers.items()])
-        return headers+'\n\n'+self.body
+        headers = '\n'.join(["{:s}: {:s}".format(k, v)
+                             for k, v in self.headers.items()])
+        return headers + '\n\n' + self.body
 
     def __str__(self):
         """
@@ -83,20 +87,18 @@ class Response(object):
         """
         return self.make_output().encode('utf-8')
 
+
 def get_htmltemplate():
     """
     レスポンスとして返すHTMLのうち，定型部分を返す
     """
-    html_body = u"""
-    <html>
+    html_body = """
+    <html lang="ja">
       <head>
-        <meta http-equiv="content-type"
-              content="text/html;charset=utf-8" />
+        <meta charset="utf-8">
       </head>
       <body>
-      %s
+      {:s}
       </body>
     </html>"""
     return html_body
-
-
--- sample_codes/chapter06/cgi-bin/rssreader1.py
+++ sample_codes/chapter06/cgi-bin/rssreader1.py
@@ -3,33 +3,34 @@
 
 from rssparser import parse_rss
 from httphandler import Request, Response, get_htmltemplate
-import cgitb; cgitb.enable()
+import cgitb
+cgitb.enable()
 
-form_body=u"""
+form_body = """
   <form method="POST" action="/cgi-bin/rssreader1.py">
     RSSのURL:
-    <input type="text" size="40" name="url" value="%s"/>
+    <input type="text" size="40" name="url" value="{:s}/>
     <input type="submit" />
   </form>"""
 
-rss_parts=u"""
-<h3><a href="%(link)s">%(title)s</a></h3>
-<p>%(description)s</p>
+rss_parts = """
+<h3><a href="{link:s}">{title:s}</a></h3>
+<p>{description:s}</p>
 """
 
-content=u"URLを入力してください"
-req=Request()
+content = "URLを入力してください"
+req = Request()
 if req.form.has_key('url'):
     try:
-        rss_list=parse_rss(req.form['url'].value)
-        content=""
+        rss_list = parse_rss(req.form['url'].value)
+        content = ""
         for d in rss_list:
-            content+=rss_parts%d
+            content += rss_parts.format(d)
     except:
         pass
 
-res=Response()
-body=form_body%req.form.getvalue('url', '')
-body+=content
-res.set_body(get_htmltemplate()%body)
-print res
+res = Response()
+body = form_body.format(req.form.getvalue('url', ''))
+body += content
+res.set_body(get_htmltemplate().format(body))
+print(res)
